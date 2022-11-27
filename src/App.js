6353ceff9d78b5c84ab19722fb5576922c655a5b
Sachin <vsachin220@gmail.com>
2022-11-27 09:36:42 +0530

    formatting code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-
-import './App.css';
-import {BrowserRouter as Router,Routes,Route,Link, useNavigate} from 'react-router-dom'
-import Home from './pages/Home';
-import CreatePost from './pages/createpost';
-import Login from './pages/Login';
-import {useState} from 'react';
-import {signOut} from 'firebase/auth'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/createpost";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
-import {useEffect} from 'react'
 function App() {
-  const [isAuth,setIsAuth]=useState(false);
-   
+  const [isAuth, setIsAuth] = useState(false);
+
   //const navigate = useNavigate();
-  const signUserOut = () =>{
-    signOut(auth).then(()=>{
+  const signUserOut = () => {
+    signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
-      window.location.pathname='/'
-    })
-  }
+      window.location.pathname = "/";
+    });
+  };
   return (
     <Router>
       <nav>
         <Link to="/home">Home</Link>
-        
-        {!isAuth ?(<Link to="/">Login</Link>):
-        (<>
-        <Link to="/createpost">Createpost</Link>
-        <button onClick={signUserOut}>Log Out</button>
-        </>)}
+
+        {!isAuth ? (
+          <Link to="/">Login</Link>
+        ) : (
+          <>
+            <Link to="/createpost">Createpost</Link>
+            <button onClick={signUserOut}>Log Out</button>
+          </>
+        )}
       </nav>
       <Routes>
-        <Route path='/home' element={<Home isAuth={isAuth}/>}/>
-        <Route path='/createpost' element={<CreatePost isAuth={isAuth}/>}/>
-        <Route path='/' element={<Login setIsAuth={setIsAuth}/>}/>
+        <Route path="/home" element={<Home isAuth={isAuth} />} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,60 @@
-import React,{ useEffect, useState } from "react";
-import {collection, deleteDoc, doc, getDocs} from 'firebase/firestore'
-import {auth,db} from'../firebase-config'
-import {useNavigate} from 'react-router-dom';
-function Home({isAuth})
-{ 
-    const email = auth.currentUser.email;
-    const arr = email.split('@');
-    const domain = arr[1];
-    const[postLists,setPostList]=useState([])
-    const postsCollectionRef = collection(db,domain);
-    useEffect(() => {
-        const getPosts = async()=>
-        {
-            const data = await getDocs(postsCollectionRef)
-            setPostList(data.docs.map((doc)=>
-            ({
-                ...doc.data(),
-                id : doc.id
-            })));
-        };
-        getPosts();
-    }, []);
+import React, { useEffect, useState } from "react";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+function Home({ isAuth }) {
+  const email = auth.currentUser.email;
+  const arr = email.split("@");
+  const domain = arr[1];
+  const [postLists, setPostList] = useState([]);
 
-    
-    const deletePost = async (id) =>
-    {
-        const postDoc = doc(db,domain,id)
-        await deleteDoc(postDoc)
-    }
-    return(
-        <div className="homePage">
-            {postLists.map((post)=>{
-            return (<div className="post">
-                <div className="postHeader">
-                    <div className="title">
-                        <h1>{post.title}</h1>
-                    </div>
-                    <div className="deletePost">
-                        {isAuth && post.author.id===auth.currentUser.uid&&(<button onClick={()=>{
-                            deletePost(post.id)
-                        }}>&#128465;</button>)}
-                        </div>
-                </div>
-                <div className="postTextContainer">{post.posttext}</div>
-                <h3>@{post.author.name}</h3>
-            </div>)
-        })}
-        </div>
-    )
+  useEffect(() => {
+    const getPosts = async () => {
+      const domainName = domain;
+      const postsCollectionRef = collection(db, domainName);
+      
+      const data = await getDocs(postsCollectionRef);
+      setPostList(
+        data.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
 
+    getPosts();
+  }, []);
+
+  const deletePost = async id => {
+    const postDoc = doc(db, domain, id);
+    await deleteDoc(postDoc);
+  };
+  return (
+    <div className="homePage">
+      {postLists.map(post => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer">{post.posttext}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 export default Home;
